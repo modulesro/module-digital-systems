940af351e8084e6033774c3f07b68a232af42e9d
Branislav Balaz <branislav.balaz@xit.camp>
2025-02-11 12:20:55 +0100

    comments

--- duplicates.rb
+++ duplicates.rb
@@ -29,6 +29,7 @@ def process_file(file, initial_file_hash_map)
   end
 end
 
+# looks for duplicated files, every value in filtered_file_hash_map contains more than one file
 def find_duplicate_files(filtered_file_hash_map)
   duplicates_hash_map = {}
   for entry in filtered_file_hash_map
@@ -37,6 +38,9 @@ def find_duplicate_files(filtered_file_hash_map)
   duplicates_hash_map
 end
 
+# compare all files with same MD5 hash and if there are duplicate files adds them to new map with MD5 hash as key
+# files are duplications if they have same MD5, they have same size and they have same content
+# for optimization comparison for content happens only if files have same size
 def process_entry(entry, duplicates_hash_map)
   key = entry[0]
   value = entry[1]
@@ -54,6 +58,7 @@ def process_entry(entry, duplicates_hash_map)
   duplicates_hash_map[key] = file_set.to_a
 end
 
+# prints grouped duplicates with hash and file names. if no duplicates found prints message about that
 def print_duplicates(duplicates_hash_map)
   if duplicates_hash_map.empty?
     puts "No duplicates found."
@@ -69,11 +74,12 @@ def print_duplicates(duplicates_hash_map)
   end
 end
 
-# first program input argument must be existing root directory path (e.g. C:/)
+# first program input argument is mandatory and must be existing root directory path (e.g. C:/)
 input_root_path = ARGV[0]
-# second input program argument must be valid file mask (e.g. "*.txt")
-input_file_mask = ARGV[1]
+# second input program argument is not mandatory and must be valid file mask (e.g. "*.txt"). if not passed, default "*" for any file used
+input_file_mask = ARGV[1] || "*"
 initial_file_hash_map = create_file_hash_map(input_root_path, input_file_mask)
+# process only entries where duplicate files could occur => if only one file belongs to concrete hash it can be ignored
 filtered_file_hash_map = initial_file_hash_map.select { |key, value| value.length > 1 }
 duplicates_hash_map = find_duplicate_files(filtered_file_hash_map)
 print_duplicates(duplicates_hash_map)
